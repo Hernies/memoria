745e81f22677b05b047f18cdcdf819f16e8e514b
Hernies <hernancalvoaguiar.2@gmail.com>
2024-05-29 17:36:22 +0200

    tree y alguna pelea

--- compiletfg.sh
+++ compiletfg.sh
@@ -1,4 +1,4 @@
-pdflatex -synctex=1 -interaction=nonstopmode "tfg_etsiinf_HernanCalvo".tex
+pdflatex -synctex=1 -interaction=nonstopmode -shell-escape "tfg_etsiinf_HernanCalvo".tex
 biber "tfg_etsiinf_HernanCalvo"
-pdflatex -synctex=1 -interaction=nonstopmode "tfg_etsiinf_HernanCalvo".tex
-pdflatex -synctex=1 -interaction=nonstopmode "tfg_etsiinf_HernanCalvo".tex
+pdflatex -synctex=1 -interaction=nonstopmode -shell-escape "tfg_etsiinf_HernanCalvo".tex
+pdflatex -synctex=1 -interaction=nonstopmode -shell-escape "tfg_etsiinf_HernanCalvo".tex
